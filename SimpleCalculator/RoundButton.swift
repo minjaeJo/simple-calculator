1b84ea9c8f8e489499835a1a20c6570e75c6f0ce
minjae <alswo2722@naver.com>
2019-01-22 17:22:30 +0900

    UI 업데이트 및 IBAction & IBOulte 연결

---
+++ SimpleCalculator/RoundButton.swift
@@ -0,0 +1,30 @@
+//
+//  RoundButton.swift
+//  SimpleCalculator
+//
+//  Created by minjae on 22/01/2019.
+//  Copyright © 2019 minjae. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var is_number_in_pannel = false
+    @IBOutlet weak var display_pannel: UILabel!
+    @IBAction func touchNumber(sender: UIButton) {
+        let number = sender.currentTitle!
+        if is_number_in_pannel{
+            display_pannel.text = display_pannel.text! + number
+        }
+        else {
+            display_pannel.text = number
+            is_number_in_pannel = true
+        }
     }
-
-
+    @IBAction func performOperator(_ sender: Any) {
+        
+    }
 }
 
